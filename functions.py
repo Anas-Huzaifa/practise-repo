a3b7f0ca5b461a1139b214b61a556252bc906eb9
addyshareef <120679786+addyshareef@users.noreply.github.com>
2023-01-21 01:57:50 +0530

    Add files via upload
    getting functions on git hub

---
+++ functions.py
@@ -0,0 +1,8 @@
+def add_numbers (num1 ,num2):
+    print('hello number')
+    return num1+num2
+ 
+num1=int(input("Enter First Number:"))
+num2=int(input("Enter Second Number:"))
+print(add_numbers(num1,num2)) 
+add_numbers(5,6)   
