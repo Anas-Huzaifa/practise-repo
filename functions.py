a671bc14c12427758d3b3c18f01f6190b5598b96
Anas-Huzaifa <107434083+Anas-Huzaifa@users.noreply.github.com>
2023-02-16 20:54:14 +0530

    Delete functions.py

--- functions.py
+++
@@ -1,8 +0,0 @@
-def add_numbers (num1 ,num2):
-    print('hello number')
-    return num1+num2
- 
-num1=int(input("Enter First Number:"))
-num2=int(input("Enter Second Number:"))
-print(add_numbers(num1,num2)) 
-add_numbers(5,6)   
